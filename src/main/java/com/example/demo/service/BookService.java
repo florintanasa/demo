051ef9a1fad6e42ed319d5d21843f484ece34743
Florin Tanasă <florin.tanasa@gmail.com>
2022-08-14 21:59:43 +0300

    create service for Book entity, It contains listeners

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,37 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.vaadin.crudui.crud.CrudListener;
+
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+public class BookService implements CrudListener<Book> {
+
+    private final BookRepository repository;
+
+    @Override
+    public Collection<Book> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Book add(Book book) {
+        return repository.save(book);
+    }
+
+    @Override
+    public Book update(Book book) {
+        return repository.save(book);
+    }
+
+    @Override
+    public void delete(Book book) {
+        repository.delete(book);
+
+    }
+}
